e6d167b5ed8d3272d97a6af18f2b4808cd0cc4e7
yashkumar2603 <yash.2603kumar@gmail.com>
2025-01-23 12:55:21 +0000

    Finshed

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://evolved-grubworm-79.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
--- convex/users.ts
+++ convex/users.ts
@@ -18,7 +18,7 @@ export const syncUser = mutation({
 
     return await ctx.db.insert("users", {
       ...args,
-      role: "candidate",
+      role: "interviewer",
     });
   },
 });
